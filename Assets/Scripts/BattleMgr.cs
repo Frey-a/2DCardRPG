ef783c0ae92804637833cd0589d9c5a547d25f8d
Freya <141486472+Frey-a@users.noreply.github.com>
2025-08-20 00:03:34 +0900

    BattleScene
    씬 및 관련 스크립트

---
+++ Assets/Scripts/BattleMgr.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BattleMgr : MonoBehaviour
+{
+    public BattleUIMgr uiMgr;
+    public DicePhaseMgr diceMgr;
+
+    private void Start()
+    {
+        CreateScene();
+    }
+
+    private void CreateScene()
+    {
+        foreach(int charId in InfoMgr.Instance.getCharIds()) // ally
+        {
+            GameObject charSlot = Instantiate(InfoMgr.Instance.charPrefab, uiMgr.allies);
+            Transform character = charSlot.transform.GetChild(0);
+            CharInfo info = character.GetComponent<CharInfo>();
+
+            diceMgr.CreateSlot(charId, false);
+            info.SetData(charId);
+            uiMgr.UpdateStatus(info);
+        }
+
+        //enemy 생성
+    }
+
+    private void StartBattle()
+    {
+        diceMgr.RollDice();
+    }
+}
---
+++ Assets/Scripts/UIUX/BattleUIMgr.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleUIMgr : MonoBehaviour
+{
+    public Transform turnImgParent;
+    public Transform allies; // 캐릭터 생성 위치
+    public Transform enemies; // 적 생성 위치
+
+    public void UpdateStatus(CharInfo info)
+    {
+        Text hpTxt = info.hp.GetComponentInChildren<Text>();
+        hpTxt.text = info.GetStatus().hp + " / " + info.GetData().maxHp;
+    }
+
+    public void ChgImgAlpha()
+    {
+        List<Image> activeImges = new List<Image>();
+        int imgCnt = 4; // 보여지는 최대 갯수
+
+        foreach (Transform turnImg in turnImgParent)
+        {
+            if (turnImg.gameObject.activeSelf)
+            {
+                Image img = turnImg.GetComponent<Image>();
+
+                if (img != null)
+                {
+                    activeImges.Add(img);
+                }
+            }
+        }
+
+        if (imgCnt > activeImges.Count)
+        {
+            imgCnt = activeImges.Count;
+        }
+
+        for (int i = 0; i < imgCnt; i++)
+        {
+            float alpha = Mathf.Lerp(1f, 0.1f, i * 0.3f);
+            Image img = activeImges[i];
+
+            if (img != null)
+            {
+                Color color = img.color;
+                color.a = alpha;
+                img.color = color;
+            }
+        }
+    }
+}
